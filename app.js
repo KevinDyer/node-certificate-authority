2218787805f845450023f1c5e8c47848f13f3d15
Kevin Dyer <kevin.m.dyer@gmail.com>
2017-06-10 06:14:20 -0600

    Added CA router.

--- app.js
+++ app.js
@@ -3,11 +3,22 @@
 
   const RootCA = require('./lib/ca/root');
   const IntermediateCA = require('./lib/ca/intermediate');
+  const CARouter = require('./lib/routers/ca');
+  const Server = require('./lib/server');
+
+  const server = new Server();
 
   const root = new RootCA();
+  const rootRouter = new CARouter(root);
+  server.use('/root', rootRouter.getRouter());
+
   const intermediate = new IntermediateCA();
+  const intermediateRouter = new CARouter(intermediate);
+  server.use('/intermediate', intermediateRouter.getRouter());
 
   Promise.resolve()
   .then(() => root.initialize())
-  .then(() => intermediate.initialize(root));
+  .then(() => intermediate.initialize(root))
+  .then(() => server.initialize(intermediate))
+  .then(() => server.listen({port: 9000}));
 })();
