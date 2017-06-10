c2c8b88378943b445eac2c610da5492e7ce285b2
Kevin Dyer <kevin.m.dyer@gmail.com>
2017-06-09 21:30:52 -0600

    Added roto and intermediate CA class.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# node-certificate-authority
-A smallish node service that acts a certificate authority
+Certificate Authority (CA)
+===
+A small-ish, simple-ish Node.js service that acts an intermediate certificate authority (CA).
+
+### Background
+By design, this service is not meant to be a root CA. It is designed to be an intermediate CA. The service may initialize under one of the following conditions:
+1. The key and certificate for the intermediate CA are present.
+2. The key and certificate for the root CA are presented (not a good idea).
+3. If neither are present, a key and CSR are generated and made available to `GET`. An endpoint is also made available to upload the certificate.
+
+### Thanks to
+- [OpenSSL Certificate Authority](https://jamielinux.com/docs/openssl-certificate-authority/index.html)
+- [OpenSSL Essentials: Working with SSL Certificates, Private Keys and CSRs](https://www.digitalocean.com/community/tutorials/openssl-essentials-working-with-ssl-certificates-private-keys-and-csrs)
+- [Dexus/pem](https://github.com/Dexus/pem) (github)
--- app.js
+++ app.js
@@ -1,56 +1,13 @@
-const PORT = process.env.PORT || 8080;
-const SERVER_KEY_FILEPATH = process.env.SERVER_KEY_FILEPATH || 'test.key.pem';
-const SERVER_CERT_FILEPATH = process.env.SERVER_CERT_FILEPATH || 'test.cert.pem';
+(() => {
+  'use strict';
 
-const fs = require('fs');
-const https = require('https');
-const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const compression = require('compression');
-const bodyParser = require('body-parser');
-const pem = require('pem');
+  const RootCA = require('./lib/ca/root');
+  const IntermediateCA = require('./lib/ca/intermediate');
 
-// TODO: If no server key or server cert generate using pem
-// TODO: Better csr verification
-// TODO: Make better input for server key and server cert
-// TODO: Add config/extension file?
+  const root = new RootCA();
+  const intermediate = new IntermediateCA();
 
-const serverKey = fs.readFileSync(SERVER_KEY_FILEPATH);
-const serverCert = fs.readFileSync(SERVER_CERT_FILEPATH);
-let serial = 0;
-
-const app = express();
-app.use(helmet());
-app.use(morgan('common'));
-app.use(compression());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.get('/', (req, res) => {
-  res.status(200).json({healthy: true});
-});
-app.post('/', (req, res) => {
-  const csr = req.body.csr || 'this is not a csr';
-  const options = {
-    serverKey: serverKey,
-    serverCert: serverCert,
-    serial: serial++,
-    csr: csr
-  };
-  pem.createCertificate(options, (err, result) => {
-    if (err) {
-      res.status(500).json({success: false, error: 'failed to sign csr'});
-    } else {
-      res.status(200).json({success: true, data: {certificate: result.certificate}});
-    }
-  });
-});
-
-const options = {
-  key: serverKey,
-  cert: serverCert
-};
-const server = https.createServer(options, app);
-server.listen(PORT, () => {
-  console.log(`Listening on ${PORT}.`);
-});
+  Promise.resolve()
+  .then(() => root.initialize())
+  .then(() => intermediate.initialize(root));
+})();
